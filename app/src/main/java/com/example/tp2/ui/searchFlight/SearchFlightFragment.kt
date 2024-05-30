04cb2186d37aba7b2e700ee34f8fef1f844a6d9d
ivo gimenez <sgimenezivan@gmail.com>
2024-05-30 14:25:50 -0300

    feat: add search to navigation

--- app/src/main/java/com/example/tp2/ui/searchFlight/SearchFlightFragment.kt
+++ app/src/main/java/com/example/tp2/ui/searchFlight/SearchFlightFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.tp2.R
 
 class SearchFlightFragment : Fragment() {
@@ -17,7 +19,11 @@ class SearchFlightFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_flight, container, false)
+        val view = inflater.inflate(R.layout.fragment_search_flight, container, false);
+        val btnSearch = view.findViewById<Button>(R.id.btnSearchFlights)
+        btnSearch.setOnClickListener {
+            findNavController().navigate(R.id.action_searchFlightFragment_to_flightListFragment)
+        }
+        return view;
     }
 }
