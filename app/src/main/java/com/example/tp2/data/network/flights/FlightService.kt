731597532fc2e3346fe3d40f33545cb9e6a14a83
Lucky <lucaslois95@gmail.com>
2024-06-02 16:55:55 -0300

    refactor: :recycle: Using ViewModels and ViewBinding in DetailsFragment

--- app/src/main/java/com/example/tp2/data/network/flights/FlightService.kt
+++ app/src/main/java/com/example/tp2/data/network/flights/FlightService.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 class FlightService {
     var retroFitClient: FlightApiClient
