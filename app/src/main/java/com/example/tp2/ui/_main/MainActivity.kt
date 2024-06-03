cd655edfbdd42718e395803dc925c98a58ba7201
mtadakuma <tadakumamiguelangel@gmail.com>
2024-06-02 22:51:32 -0300

    drawer menu

--- app/src/main/java/com/example/tp2/ui/_main/MainActivity.kt
+++ app/src/main/java/com/example/tp2/ui/_main/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.tp2.ui._main
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
@@ -27,6 +32,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavView: BottomNavigationView
     private lateinit var topToolbar: MaterialToolbar
     private lateinit var leftNavView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     private val fragmentsNavigation = setOf(
         R.id.homeFragment,
@@ -38,6 +45,7 @@ class MainActivity : AppCompatActivity() {
     )
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -45,7 +53,8 @@ class MainActivity : AppCompatActivity() {
 
         topToolbar = findViewById(R.id.customToolbar)
         bottomNavView = findViewById(R.id.bottomNav)
-        leftNavView = findViewById(R.id.navigation_view)
+        leftNavView = findViewById(R.id.navigationView)
+        drawerLayout = findViewById(R.id.drawerLayout)
 
         setSupportActionBar(topToolbar)
         navController = findNavController(R.id.navGraphFragmentActivityMain)
@@ -58,10 +67,36 @@ class MainActivity : AppCompatActivity() {
         topToolbar.setupWithNavController(navController, appBarConfiguration)
         bottomNavView.setupWithNavController(navController)
 
+
+        /*
+        //Configuro NavigationView
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        leftNavView.setupWithNavController(navController)
+
+
+        toggle = ActionBarDrawerToggle(
+            this, drawerLayout, topToolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawerLayout.closeDrawers()
+            true
+        }
+
+        val btnSearch = findViewById<Button>(R.id.btnSearchFlights)
+        btnSearch.setOnClickListener {
+            findNavController().navigate(R.id.action_searchFlightFragment_to_flightListFragment)
+        }
+
+         */
+
         navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
 
-        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
-        NavigationUI.setupWithNavController(leftNavView, navHostFragment.navController)
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+        NavigationUI.setupWithNavController(leftNavView, navController)
 
             when (destination.id) {
                 R.id.searchFlightFragment,
@@ -81,4 +116,27 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    /*
+
+    override fun onSupportNavigateUp(): Boolean {
+        //Fuezo al boton de navegación de la toolbar que solo abra el menú Drawer
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return if (toggle.onOptionsItemSelected(item)) {
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+     */
 }
